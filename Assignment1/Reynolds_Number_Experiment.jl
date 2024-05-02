165c9f43edafeb25d3e6e8d3ef462f6179a76bfa
ABEngineer2000 <jbenne26@byu.edu>
2024-05-02 12:39:04 -0600

    Added A Plotter Function

--- Assignment1/Reynolds_Number_Experiment.jl
+++ Assignment1/Reynolds_Number_Experiment.jl
@@ -106,11 +106,36 @@ function ReynoldsRepeater(ReynoldsRange, AlphaRange, DeltaAlpha, n_pan, iteratio
     end
 end
 
-ReynoldsRepeater([1000 10000 1e5 1e6 1e7], [0 3], 0.5, 100, 100, "Assignment1\\NACA16-006")
+function plotter(CSVFile, filename)
+    Array_plot = readdlm(CSVFile, ',')
+    n = size(Array_plot, 1) - 1
+    Header = Array_plot[1, :]
+    Alpha = Array_plot[2:n, 1]
+    c_l = Array_plot[2:n, 2]
+    c_d = Array_plot[2:n, 3]
+    c_m = Array_plot[2:n, 4]
+    c_lplot = plot(Alpha, c_l, label=false, xlabel="Angle of Attack (degrees)", ylabel="Lift Coefficient")
+    c_dplot = plot(Alpha, c_d, label=false, xlabel="Angle of Attack (degrees)", ylabel="Drag Coefficient")
+    c_mplot = plot(Alpha, c_m, label=false, xlabel="Angle of Attack (degrees)", ylabel="Moment Coefficient")
+    savefig(c_lplot, "$(filename)_lift_plot.png")
+    savefig(c_dplot,"$(filename)_Drag_plot.png")
+    savefig(c_mplot,"$(filename)_Moment_plot.png")
+end
 
+plotter("Assignment1\\NACA16-006Reynolds_Number10000.0.csv", "Assignment1\\NACA16-006Reynolds_Number10000.0")
+
+#= 
+A = [1 2 3 4; 5 6 7 8; 9 10 11 12]
+println(A[:, 2:4 ])
+=#
+
+#=
+ReynoldsRepeater([1000 10000 1e5 1e6 1e7], [0 3], 0.5, 100, 100, "Assignment1\\NACA16-006")
+=#
 #call function here
 #=
 alpha, c_l, c_d, c_m, converged, CSVArray, CSVHeader = ReyonldsExperiment(1e5, [0 3], 0.5, 100, 100)
 WriteFile(CSVArray, "Assignment1\\Test1.csv", CSVHeader)
 println("")
 =#
+println("")
