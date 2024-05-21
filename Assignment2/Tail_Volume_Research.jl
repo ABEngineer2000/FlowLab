1066bbfd7754567deb9f1a04221976f209c5bb56
ABEngineer2000 <jbenne26@byu.edu>
2024-05-21 10:47:56 -0600

    Added some Tail Volume Research

--- Assignment2/Tail_Volume_Research.jl
+++ Assignment2/Tail_Volume_Research.jl
@@ -29,6 +29,10 @@ function Tail_Analysis(chord_length, wingspan, HS_chord, HS_span, VS_chord, VS_s
 
         #compute tail volume coefficient. See page 94 Flight Vehicle Design
         Tail_Volume_Coefficient = (HS_span * HS_chord)/(wingspan * chord_length) #this is based on pitch stability
+
+        #computes trim stability
+        Cmg = Pitch_Trim_Stability(wCMy, wCFz, hCMy, hCFz, wing_distance, tail_distance, alpha_range, Tail_Volume_Coefficient,n, HS_chord)
+
         #compute stability derivatives. Note that this is assuming the cmy moments oppose the moments created by clz
         DCmg_Dalpha = Array{Float64, 1}(undef, length(wing_dclz_dalpha))
         for i = 1:length(wing_dclz_dalpha)
@@ -40,7 +44,7 @@ function Tail_Analysis(chord_length, wingspan, HS_chord, HS_span, VS_chord, VS_s
         CSV[:, 2] = DCmg_Dalpha
         CSV_Header = ["Alpha (Degrees)" "DCmg_Dalpha"]
         WriteFile(CSV, filename, CSV_Header)
-        return Tail_Volume_Coefficient, DCmg_Dalpha #returns tail volume coefficient and main stability derivative
+        return Tail_Volume_Coefficient, DCmg_Dalpha, Cmg #returns tail volume coefficient and main stability derivative
 end
 
 function Tail_Volume_Coefficient_Repeater(chord_length, wingspan, HS_chord, HS_span, VS_chord, VS_span, tail_distance, wing_distance, n, alpha_range, filename, Tail_Volume_Scalars) #This function will call Tail_Analysis for ever tail volume coefficient you want to analyze
@@ -53,17 +57,27 @@ function Tail_Volume_Coefficient_Repeater(chord_length, wingspan, HS_chord, HS_s
     HS_chord1 = Array{Float64, 1}(undef, length(Tail_Volume_Scalars))
     Tail_Volume_Coefficient = Array{Float64, 1}(undef, length(Tail_Volume_Scalars))
     DCmg_Dalpha = Array{Float64, 2}(undef, length(alpha_range), length(Tail_Volume_Scalars))
+    Cmg = Array{Float64, 2}(undef, length(alpha_range), length(Tail_Volume_Scalars)) #This is for trim stability
     plot() #resets plot
     for i =1:length(Tail_Volume_Scalars)
         HS_span1[i] = HS_span * Tail_Volume_Scalars[i] #Creates new Horizontal Stabilizer coefficients based on the Tail Volume Scalars
         HS_chord1[i] = HS_chord * Tail_Volume_Scalars[i]
         Tail_Volume_Coefficient[i] = (HS_span1[i] * HS_chord1[i])/(wingspan * chord_length) #Computes new tail volume coefficient
         filename1[i] = "$(filename)_TailVolumeCoefficient_$(Tail_Volume_Coefficient[i])"
-        Tail_Volume_Coefficient[i], DCmg_Dalpha[:, i] = Tail_Analysis(chord_length, wingspan, HS_chord1[i], HS_span1[i], VS_chord, VS_span, tail_distance, wing_distance, n, alpha_range, "$(filename1[i]).csv")
+        Tail_Volume_Coefficient[i], DCmg_Dalpha[:, i], Cmg[:, i] = Tail_Analysis(chord_length, wingspan, HS_chord1[i], HS_span1[i], VS_chord, VS_span, tail_distance, wing_distance, n, alpha_range, "$(filename1[i]).csv")
         plot1 = plot!(alpha_range, DCmg_Dalpha[:, i], label= "Tail Volume Coefficient: $(Tail_Volume_Coefficient[i])", xlabel="Alpha(degrees)", ylabel="Stability Derivative")
         savefig(plot1, "$(filename).png")
     end
-
+    plot()
+    for i = 1:length(Tail_Volume_Scalars) #plot trim stability for each tail volume scalar
+        plot2 = plot!(alpha_range, Cmg[:, i], label= "Tail Volume Coefficient: $(Tail_Volume_Coefficient[i])", xlabel = "Alpha (degrees)", ylabel = "Cmg")
+        savefig(plot2, "$(filename)_TrimStability.png")
+        CSV_Header = ["Alpha_Range" "Cmg"]
+        CSV_Array = Array{Float64, 2}(undef, length(alpha_range), 2)
+        CSV_Array[:, 1] = alpha_range
+        CSV_Array[:, 2] = Cmg[:, i]
+        WriteFile(CSV_Array, "$(filename)_TVC_$(Tail_Volume_Coefficient[i])TrimStability.csv", CSV_Header)
+    end
 
 
 end
@@ -102,7 +116,20 @@ function VLM_AlphaRange(Wingspan, chord_length, alpha_range) #This function perf
         CFx[i], CFy[i], CFz[i], Cmx[i], Cmy[i], Cmz[i], CDiff[i] = VLM(Wingspan*chord_length, chord_length, Wingspan,alpha_range[i]*pi/180 ,0.0, [0.0; 0.0; 0.0], 1) #Assuming beta and omega are 0 and it also converts alpha_range into radians
     end
     return CFx, CFy, CFz, Cmx, Cmy, Cmz, CDiff #returns the coefficients for force and moment in cartesian coordinates
+end
 
+function Pitch_Trim_Stability(Cmw, Clw, Cmt, Clt, wing_distance, tail_distance, alpha_range, Tail_Volume_Coefficient, n, wing_chord) #This function checks trim stability at the given angles of attack. It does this
+    #by computing the Moment Coefficient about the center of gravity.
+    #It outputs a graph/CSV containing the moment coefficient about the center of gravity.
+    #This utilizes equation 5.23 in the flight vehicle design textbook
+    #This assumes there is a tail
+    #inputs are Moment and lift coefficients for the wing and tail. Also the distance from the aerodynamic center of the tail and wing to the center of gravity.
+    #Other inputs are the tail volume coefficient, wing efficiency, and wing chord length
+    Cmg = Array{Float64, 1}(undef, length(alpha_range))
+    for i = 1:length(Cmg)
+        Cmg[i] = Cmw[i] + Cmt[i] - (wing_distance/wing_chord)*Clw[i] - n*Tail_Volume_Coefficient*Clt[i]*tail_distance #This is just equation 5.23 in the flight vehicle design textbook
+    end
+    return Cmg
 end
 
 function VLM(Sref, cref, bref, alpha, beta, omega, Vinf_in) #Performs a Vortex lattice analysis
@@ -154,7 +181,20 @@ function RangeMaker(range, stepsize) #creates an array of values given the range
 end
 
 #Tail_Analysis(4, 10, 2, 4, 3, 4, 7, 2, 0.85, RangeMaker([-1 1], 0.01), "Assignment2\\TailTest.csv") #This was for testing the Tail_Analysis funtion
-Tail_Volume_Coefficient_Repeater(4, 10, 2, 4, 3, 4, 7, 2, 0.85, RangeMaker([-1 1], 0.01), "Assignment2\\Tail_Volume_Research_Docs\\Test1", [1 2])
+#This is my call that I've been using for research
 
+chord_length = 4
+wingspan = 10
+HS_chord = 0.5
+HS_span = 2
+VS_chord = 3 
+VS_span = 4
+tail_distance = 10
+wing_distance = 0
+n = 1
+alpha_range  = RangeMaker([-1 1], 0.05)
+filename = "Assignment2\\Tail_Volume_Research_Docs\\Test9"
+Tail_Volume_Scalars = [1 2]
+Tail_Volume_Coefficient_Repeater(chord_length, wingspan, HS_chord, HS_span, VS_chord, VS_span, tail_distance, wing_distance, n ,alpha_range, filename, Tail_Volume_Scalars)
 
 println("") #I add this here so it won't print anything unless I need it to
