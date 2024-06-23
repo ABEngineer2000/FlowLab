00bcd908e1b7f2d3d8fe76a41f27c2df9290f7f8
ABEngineer2000 <jbenne26@byu.edu>
2024-06-22 19:00:35 -0600

    Started my Final Project Code and Made a Plan

--- FinalProject/FinalProject.jl
+++ FinalProject/FinalProject.jl
@@ -15,3 +15,65 @@ The wing will be made of a wing, horizontal, and vertical stabilizer.
 Design variables will be the chord lengths for the stabilizers.
 =#
 
+function VLM(leading_edge_distribution, chord_distribution, span) #Performs a Vortex lattice analysis
+    #this function requires two distribution vecotrs which contain the 2d wing geometry.
+    xle = Array{Float64, 1}(undef, length(chord_distribution))
+    yle = Array{Float64, 1}(undef, length(chord_distribution))
+    zle = Array{Float64, 1}(undef, length(chord_distribution))
+    chord = Array{Float64, 1}(undef, length(chord_distribution))
+    theta = Array{Float64, 1}(undef, length(chord_distribution))
+    phi = Array{Float64, 1}(undef, length(chord_distribution))
+    panel_area = Array{Float64, 1}(undef, length(chord_distribution))
+    for i = 1:length(chord_distribution)
+        xle[i] = leading_edge_distribution[i] #each leading edge is according to the input leading edge distribution vector 
+        yle[i] = (i - 1)*span/length(chord_distribution) #spanwise placement of the panels. Each planel is placed according to its top left corner
+        zle[i] = 0.0 #vertical direction, I will assume a flat wing so my z coordinate is 0.
+        chord[i] = chord_distribution[i] + 0.0 #first number is chord at the fueslage, the next is the chord at the wingtip.
+        theta[i] = 0.0 #this is twist (rotation about y-axis) at the fueslage and wingtip respectively.
+        phi[i] = 0.0 #This is rotation about the x-axis.
+        panel_area[i] = chord_distribution[i] * span/(length(chord_distribution) * 2) #outputs the area for each panel
+    end
+    fc = fill((xc) -> 0, length(chord_distribution)) # camberline function for each section, it creates a camber in the z direction. make the function in terms of xc
+    beta = 0.0
+    alpha = 5*pi/180 #set the angle of attack to 5 degrees
+
+    Panels_span = length(xle)
+    Panels_chord = 10
+    Spacing_type_span = Cosine()
+    Spacing_type_chord = Uniform()
+    Rref = [0.0,0.0,0.0]
+    Vinf = 1.0
+    wing_area = sum(panel_area)*2 #this gives wing area for lift calculations that I will use in the optimization
+    ref = Reference(wing_area, mean(chord_distribution), span, Rref, Vinf)
+    fs = Freestream(Vinf, alpha, beta, [0.0;0.0;0.0]) #Define freestream Parameters 
+
+    #create the surface
+    grid, surface = wing_to_surface_panels(xle, yle, zle, chord, theta, phi, Panels_span, Panels_chord; fc = fc, spacing_s = Spacing_type_span, spacing_c = Spacing_type_chord)
+    surfaces = [surface]
+
+    #perform steady state analysis
+    system = steady_analysis(surfaces, ref, fs; symmetric = true)
+    CF, CM = body_forces(system; frame=Wind()) #compute near body forces
+    CDiff = far_field_drag(system) #compute farfield drag
+    CFx, CFy, CFz = CF
+    CMx, CMy, CMz = CM
+
+    return CFx, CFy, CFz, CMx, CMy, CMz, CDiff, wing_area #, grid, surface these I added as outputs for troubleshooting
+end
+
+#I didn't want to download another julia package so I just created my own fucntion to find the mean
+function mean(x)
+    sum = 0.0
+    for i = 1:length(x)
+        sum = sum + x[i]
+    end
+    m = sum/length(x)
+    return m
+end
+
+leading_edge_distribution = [0.0, 0.0, 0.0, 0.0, 0.0]
+chord_distribution = [5.0, 5.0, 5.0, 5.0, 5.0]
+span = 10.0
+CFx, CFy, CFz, CMx, CMy, CMz, CDiff, wing_area = VLM(leading_edge_distribution, chord_distribution, span)
+
+println("") #this is so I don't print anything I don't want.
