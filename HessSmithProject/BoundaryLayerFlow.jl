fbb09547ffa794046f22308fd046752b95728ac6
ABEngineer2000 <jbenne26@byu.edu>
2024-10-17 20:51:20 -0600

    Added RK4 ODE Solver to Laminar Flow Solver

--- HessSmithProject/BoundaryLayerFlow.jl
+++ HessSmithProject/BoundaryLayerFlow.jl
@@ -1,4 +1,4 @@
-using DelimitedFiles, FiniteDifferences
+using DelimitedFiles, ForwardDiff
 
 import FLOWFoil.AirfoilTools
 
@@ -24,6 +24,7 @@ Lift and drag values will then be predicted from these new panels.
 
 
 function compute_laminar_delta(
+    x,
     Ve;
     ν = 0.0000148 #this is nu by the way not v, it is the kinematic viscosity of air (or fluid moving around body). It will change depending on the temperature
 )
@@ -34,19 +35,33 @@ function compute_laminar_delta(
     δ_star = similar(dve_dx, n)
 
     #Compute dve_dx for each panel
+    #for now I will be taking simple derivatives
     for i = 1:n
         if i == 1
-            
+            h = x.Panel_mid_points[i + 1][1] - x.Panel_mid_points[i][1]
+            dve_dx[i] = (Ve[i + 1] - Ve[i]) / h #forward derivative
         elseif i == n
-
+            h = (x.Panel_mid_points[i + 1] - x.Panel_mid_points[i - 1])
+            dve_dx[i] = (Ve[i + 1] - Ve[i - 1]) / h
+            #central derivative
         else
-
+            h = x.Panel_mid_points[i] - x.Panel_mid_points[i - 1]
+            dve_dx[i] = (Ve[i] - Ve[i]) / h
         end
     end
+
     #compute initial condition using equation 3.100
-    θ_0 = sqrt(0.075*ν / dve_dx[1])
+    θ[1] = sqrt(0.075*ν / dve_dx[1])
     
     #solve equation 3.95 numerically for θ, likely using the classic Runga-Kutta method
+    for i = 1:n - 1
+        h = x.Panel_mid_points[i + 1][1] - x.Panel_mid_points[i][1]
+        k1 = (0.225*ν) / (Ve[i]*θ[i]) - (3*θ_0 / Ve[i])*dve_dx[i]
+        k2 = (0.225*ν) / (Ve[i]*(θ[i] + 0.5*k1*h)) - (3*(θ[i] + 0.5*k1*h) / Ve[i])*dVe_dx[i]
+        k3 = (0.225*ν) / (Ve[i]*(θ[i] + 0.5*k2*h)) - (3*(θ[i] + 0.5*k2*h) / Ve[i])*dVe_dx[i]
+        k4 = (0.225*ν) / (Ve[i]*(θ[i] + k3*h)) - (3*(θ[i] + k3*h) / Ve[i])*dVe_dx[i]
+        θ[i + 1] = θ[i] + (1/6)*(k1 + 2*k2 + 2*k3 + k4)*h
+    end
 
     #compute δ* from H and θ (page 98 has the equation that relates the three variables)
 
@@ -54,6 +69,7 @@ function compute_laminar_delta(
 
     return δ_star
 end
+
 
 
 println("")
